3ee92efa3dbac736a25350b4ef56820da0e77f79
Natalie <Natalie.hanz@gmail.com>
2014-02-16 01:44:41 -0800

    Binary Tree Level Order Traversal

---
+++ levelOrder.py
@@ -0,0 +1,47 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrder(self, root):
+        levels = []
+        if root==None:
+            return levels
+        preLevel = []
+        preNodes = []
+        curLevel = []
+        curNodes = []
+        preNodes.append(root)
+        preLevel.append(root.val)
+        levels.append(preLevel)
+        while True:
+            while len(preNodes)>0:
+                parent = preNodes.pop(0)
+                if parent.left:
+                    curNodes.append(parent.left)
+                    curLevel.append(parent.left.val)
+                if parent.right:
+                    curNodes.append(parent.right)
+                    curLevel.append(parent.right.val)
+                if not parent.left and not parent.right:
+                    continue
+            if curNodes:
+                levels.append(curLevel)
+                preNodes = curNodes
+                curLevel = []
+                curNodes = []
+                
+                
+            else:
+                break
+        
+        return levels
+            
+            
+            
+        
