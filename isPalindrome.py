8d91fbdd687e96a3c70bde40659bef7c9f948811
Natalie <Natalie.hanz@gmail.com>
2014-02-20 01:06:41 -0800

    Valid Palindrome

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,15 +1,36 @@
 class Solution:
+    # @param s, a string
     # @return a boolean
-    def isPalindrome(self, x):
-        xstr = str(x)
+    def isPalindrome(self, s):
+        slist = self.alphanumOnly(s)
+        if len(slist) == 0:
+            return True
         i = 0
-        j = len(xstr) -1
+        j = len(slist)-1
         while i<=j:
-            if xstr[i] !=xstr[j]:
+            if slist[i] != slist[j]:
                 return False
             i += 1
             j -= 1
         return True
         
+    def alphanumOnly(self, s):
+        ss = []
+        if s == '':
+            return ss
+        # build a set
+        naset = set()
+        for i in range(10):
+            naset.add(ord('0') + i)
+        alphaset = ()
+        for j in range(26):
+             naset.add(ord('a') + j)
         
-      
+        # check any str in the set or not
+        for i in range(len(s)):
+            if ord(s[i].lower()) in naset:
+                ss.append(s[i].lower())
+        return ss
+        
+            
+        
